4a7f6d2077a5b01f1bb7f507c65fefc650cfa2df
remotequack <remotequack@gmail.com>
2021-12-15 13:57:51 +0400

    fix windows

--- manipimage/main.c
+++ manipimage/main.c
@@ -8,33 +8,39 @@
 
 #ifdef _WIN32
     #include <conio.h>
+    #include <Windows.h>
     #define FLECHE_HAUT 72
     #define FLECHE_GAUCHE 75
     #define FLECHE_DROITE 77
     #define FLECHE_BAS 80
+    #define TOUCHE_ENTRER '\r'  
 #else
     #include "consoleunix.h"
     #define FLECHE_HAUT 65
     #define FLECHE_BAS 66
     #define FLECHE_DROITE 67
     #define FLECHE_GAUCHE 68
+    #define TOUCHE_ENTRER '\n'
 #endif
 
-#define TOUCHE_ENTRER '\n'  
 #define ASCII_0 48
 
-int menu(char** choix, int nbChoix) 
+#ifndef _WIN32
+
+int menu(char** choix, int nbChoix)
 {
+    //////////////////////https://www.csie.ntu.edu.tw/~r92094/c++/VT100.html
+
     printf("\x1b[6n"); //requete coordonnées
 
     int origineConsole = 0;
     char str[8];
 
-    for(int i = 0; i < 8; i++)
+    for (int i = 0; i < 8; i++)
     {
         str[i] = _getch();
-        if(str[i] == 'R') 
-        { 
+        if (str[i] == 'R')
+        {
             str[i] = 0;
             break;
         }
@@ -49,14 +55,12 @@ int menu(char** choix, int nbChoix)
         printf("%d. %s\n", i + 1, choix[i]);
     }
 
-    //////////////////////https://www.csie.ntu.edu.tw/~r92094/c++/VT100.html
 
-    printf("0. \x1B[7m%s\n", choix[nbChoix - 1]);
+    printf("0. \x1B[7m%s\n", choix[nbChoix - 1]); //7m = inversion couleurs arière plan / premier plan
 
-    printf("\x1B[0mChoix ==> %s", choix[nbChoix - 1]);
+    printf("\x1B[0mChoix ==> %s", choix[nbChoix - 1]); //0m = rétablissement couleurs par défault
 
-
-    printf("\x1b[%d;0f", nbChoix + origineConsole);
+    printf("\x1b[%d;0f", nbChoix + origineConsole); // déplacement curseur au dernier élément du menu
 
     char selectionPred = 8;
     char selection = 8;
@@ -64,13 +68,6 @@ int menu(char** choix, int nbChoix)
 
     while (c != TOUCHE_ENTRER)
     {
-
-        /* if (c != 0)
-        {
-            printf("%d\n", c);
-            continue;
-        } */
-
         if (c >= ASCII_0 && c <= ASCII_0 + 9) // Le controle du tableau par valeur numérique ne fonctionnera pas pour des valeurs de choix superieur à 9
         {
             selection = c - ASCII_0;
@@ -102,7 +99,7 @@ int menu(char** choix, int nbChoix)
         {
             printf("\x1b[%d;4f\x1B[0m%s", selectionPred + origineConsole, choix[selectionPred - 1]);
             printf("\x1b[%d;4f\x1B[7m%s", selection + origineConsole, choix[selection - 1]);
-            printf("\x1b[%d;0f\x1B[0m\x1b[KChoix ==> %s", origineConsole + nbChoix + 1, choix[selection - 1]);
+            printf("\x1b[%d;0f\x1B[0m\x1b[KChoix ==> %s", origineConsole + nbChoix + 1, choix[selection - 1]); //K = effacer la ligne
             selectionPred = selection;
         }
 
@@ -111,6 +108,97 @@ int menu(char** choix, int nbChoix)
 
     return selection;
 }
+
+#else
+
+int menu(char** choix, int nbChoix)
+{
+    HANDLE hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    int origineConsole = 0;
+    char str[8];
+
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    BOOL ok = GetConsoleScreenBufferInfo(hConsoleOutput, &csbi);
+
+    origineConsole = csbi.dwCursorPosition.Y - 1;
+
+    for (int i = 0; i < nbChoix - 1; i++)
+    {
+        printf("%d. %s\n", i + 1, choix[i]);
+    }
+
+    printf("0. ");
+    SetConsoleTextAttribute(hConsoleOutput, csbi.wAttributes | COMMON_LVB_REVERSE_VIDEO); //inversion couleurs arière plan / premier plan
+    printf("%s\n", choix[nbChoix - 1]); 
+    SetConsoleTextAttribute(hConsoleOutput, csbi.wAttributes);
+    printf("Choix ==> %s", choix[nbChoix - 1]);
+
+    SetConsoleCursorPosition(hConsoleOutput, (COORD) { 0, nbChoix + origineConsole });
+
+    char selectionPred = 8;
+    char selection = 8;
+    char c = 0;
+
+    while (c != TOUCHE_ENTRER) 
+    {
+        while (!_kbhit()); //attend une touche
+
+        c = _getch();
+
+        if (c >= ASCII_0 && c <= ASCII_0 + 9) // Le controle du tableau par valeur numérique ne fonctionnera pas pour des valeurs de choix superieur à 9
+        {
+            selection = c - ASCII_0;
+            if (selection > nbChoix) { selection = selectionPred; }
+            if (selection == 0) { selection = nbChoix; }
+        }
+
+        switch (c)
+        {
+        case FLECHE_HAUT:
+        case FLECHE_GAUCHE:
+            if (selection != 1)
+            {
+                selection--;
+            }
+            break;
+        case FLECHE_BAS:
+        case FLECHE_DROITE:
+            if (selection < nbChoix)
+            {
+                selection++;
+            }
+            break;
+        default:
+            break;
+        }
+
+        if (selection != selectionPred)
+        {
+            SetConsoleCursorPosition(hConsoleOutput, (COORD) { 3, selection + origineConsole });
+            SetConsoleTextAttribute(hConsoleOutput, csbi.wAttributes | COMMON_LVB_REVERSE_VIDEO); //inversion couleurs arière plan / premier plan
+            printf("%s", choix[selection - 1]);
+
+            SetConsoleCursorPosition(hConsoleOutput, (COORD) { 3, selectionPred + origineConsole });
+            SetConsoleTextAttribute(hConsoleOutput, csbi.wAttributes);
+            printf("%s", choix[selectionPred - 1]);
+
+            SetConsoleCursorPosition(hConsoleOutput, (COORD) { 0, origineConsole + nbChoix + 1 });
+            printf("Choix ==> %s", choix[selection - 1]);
+
+            int predDiff = strlen(choix[selectionPred - 1]) - strlen(choix[selection - 1]);
+
+            if (predDiff) { // si le texte de l'ancienne selection est plus grande il faut effacer les caractères 'qui dépassent'
+                int n = 0;
+                FillConsoleOutputCharacterA(hConsoleOutput, ' ', predDiff, (COORD) { 10 + strlen(choix[selection - 1]), origineConsole + nbChoix + 1 }, &n);
+            }
+            selectionPred = selection;
+        }
+    }
+
+    return selection;
+}
+
+#endif
 
 int main()
 {
